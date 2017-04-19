fa9eb6090d30a68a30508c09dac843f7b2e58258
Suykoden <damasio321@gmail.com.br>
2017-04-19 20:48:38 -0300

    Alteração do modelo e da camada de view de MVC para WebAPI

---
+++ NiboChallenge.Domain/Entities/JogadorCampeao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NiboChallenge.Domain.EntitiesBase;
+
+namespace NiboChallenge.Domain.Entities
+{
+    public class JogadorCampeao : EntityBase
+    {
+        public Guid CampeaoId { get; set; }
+        public Campeao Campeao { get; set; }
+        public Guid JogadorId { get; set; }
+        public Jogador Jogador { get; set; }
+    }
+}
---
+++ NiboChallenge.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NiboChallenge.UI.WebApiApplication" Language="C#" %>
